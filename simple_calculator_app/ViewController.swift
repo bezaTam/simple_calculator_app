bb965155d053b7c08e2c820eab946b231308df7b
Beza Tameru <bbt0002@auburn.edu>
2024-06-24 09:36:28 -0500

    Initial Commit

---
+++ simple_calculator_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  simple_calculator_app
+//
+//  Created by Bezawit Tameru on 6/24/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
